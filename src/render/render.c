c35de083f45cdbb9cb1d08744c5f1c56288c3e63
itakumi-42tokyo <tigarashi@tigarashi098@gmail.com>
2026-02-24 19:40:29 +0900

    feat: add render variable

---
+++ src/render/render.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tigarashi <tigarashi@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/24 19:24:10 by tigarashi         #+#    #+#             */
+/*   Updated: 2026/02/24 19:32:20 by tigarashi        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+#include "cub3d.h"
+
+int	render(t_cub3d *app)
+{
+	double	dir_x;
+	double	dir_y;
+	double	plane_x;
+	double	plane_y;
+	double	time;
+	double	oldtime;
+
+	if (app == NULL)
+		return (-1);
+	return (0);
+}
