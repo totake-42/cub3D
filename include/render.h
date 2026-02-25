3e1bb18058df908856a03037c92cb667850b4464
itakumi-42tokyo <tigarashi@tigarashi098@gmail.com>
2026-02-25 16:07:51 +0900

    feat: add render utils functions

--- include/render.h
+++ include/render.h
@@ -6,11 +6,17 @@
 /*   By: tigarashi <tigarashi@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/25 15:13:55 by tigarashi         #+#    #+#             */
-/*   Updated: 2026/02/25 15:14:43 by tigarashi        ###   ########.fr       */
+/*   Updated: 2026/02/25 16:07:21 by tigarashi        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RENDER_H
 # define RENDER_H
+
+# include "cub3d.h"
+
+/* render utils */
+int		init_player(t_player	*player);
+void	init_ray(t_player *player, t_ray *ray, int x, int width);
 
 #endif
