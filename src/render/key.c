550fb0319ba77faca266298b8cb81e443ab38c6e
itakumi-42tokyo <tigarashi@tigarashi098@gmail.com>
2026-02-27 15:51:20 +0900

    feat: add key.c

---
+++ src/render/key.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   key.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tigarashi <tigarashi@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/27 14:46:09 by tigarashi         #+#    #+#             */
+/*   Updated: 2026/02/27 15:46:38 by tigarashi        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mlx.h"
+#include <stddef.h>
+
+#include "cub3d.h"
+
+/**
+ * @fn handle_key
+ * @brief keypress handling
+ * @param ( cub3d_ptr ) all information.
+ * @return success or failure
+ */
+
+int	handle_key(void *ptr)
+{
+	if (ptr == NULL)
+		return (-1);
+		
+}
