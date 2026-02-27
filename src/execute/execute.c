3a913be4a65d2ad7db314bfc4e2595487b5fa5d2
itakumi-42tokyo <tigarashi@tigarashi098@gmail.com>
2026-02-27 15:51:39 +0900

    feat: add execute.c

---
+++ src/execute/execute.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execute.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tigarashi <tigarashi@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/27 15:47:39 by tigarashi         #+#    #+#             */
+/*   Updated: 2026/02/27 15:50:06 by tigarashi        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mlx.h"
+#include <stddef.h>
+
+#include "cub3d.h"
+#include "status.h"
+
+t_status	execute_game(t_cub3d *app)
+{
+	void	*param;
+
+	if (app == NULL)
+		return (STATUS_ERROR);
+	if ()
+}
