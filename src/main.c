c0e1b8c6c7f2e5d5b51213a13b4527fdcdd3c4e6
Taishi Otake <totake@student.42tokyo.jp>
2026-04-17 15:41:53 +0900

    clean: split main's function

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: totake <totake@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/27 13:07:34 by itakumi           #+#    #+#             */
-/*   Updated: 2026/02/20 15:28:56 by totake           ###   ########.fr       */
+/*   Updated: 2026/04/17 14:59:03 by totake           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,49 @@
 #include "utils.h"
 #include <stdlib.h>
 
+static void	cleanup_game(t_cub3d *app)
+{
+	free_textures(app);
+	free_view(&(app->view));
+	free_map(&(app->map_data));
+}
+
+static int	init_game(t_cub3d *app, char *map_file)
+{
+	app->map_data = NULL;
+	app->view = NULL;
+	if (parse_map(app, map_file) == STATUS_ERROR)
+		return (STATUS_ERROR);
+	if (init_view(app) == STATUS_ERROR)
+	{
+		free_map(&(app->map_data));
+		return (STATUS_ERROR);
+	}
+	if (init_textures(app) == STATUS_ERROR)
+	{
+		free_view(&(app->view));
+		free_map(&(app->map_data));
+		return (STATUS_ERROR);
+	}
+	init_player(app);
+	return (STATUS_OK);
+}
+
+/*
+ * Setup MLX event hooks
+ * - mlx_loop_hook: Continuously run game_loop when idle
+ * - mlx_hook (event=2, mask=1L<<0): KeyPress event handler
+ *   KeyPressMask (1L<<0) tells X11 server to forward keyboard events
+ * - mlx_hook (event=17, mask=0): DestroyNotify event (window close button)
+ *   No mask needed for structure events
+ */
+static void	setup_hooks(t_cub3d *app)
+{
+	mlx_loop_hook(app->view->mlx_ptr, game_loop, app);
+	mlx_hook(app->view->win_ptr, 2, 1L << 0, handle_keypress, app);
+	mlx_hook(app->view->win_ptr, 17, 0, handle_close, app);
+}
+
 int	main(int argc, char **argv)
 {
 	t_cub3d	app;
@@ -29,28 +72,10 @@ int	main(int argc, char **argv)
 		ft_putendl_fd(ERROR_ARGC, STDERR_FILENO);
 		return (EXIT_FAILURE);
 	}
-	app.map_data = NULL;
-	app.view = NULL;
-	if (parse_map(&app, argv[1]) == STATUS_ERROR)
+	if (init_game(&app, argv[1]) == STATUS_ERROR)
 		return (EXIT_FAILURE);
-	if (init_view(&app) == STATUS_ERROR)
-	{
-		free_map(&(app.map_data));
-		return (EXIT_FAILURE);
-	}
-	if (init_textures(&app) == STATUS_ERROR)
-	{
-		free_view(&(app.view));
-		free_map(&(app.map_data));
-		return (EXIT_FAILURE);
-	}
-	init_player(&app);
-	mlx_loop_hook(app.view->mlx_ptr, game_loop, &app);
-	mlx_hook(app.view->win_ptr, 2, 1L << 0, handle_keypress, &app);
-	mlx_hook(app.view->win_ptr, 17, 0, handle_close, &app);
+	setup_hooks(&app);
 	mlx_loop(app.view->mlx_ptr);
-	free_textures(&app);
-	free_view(&(app.view));
-	free_map(&(app.map_data));
+	cleanup_game(&app);
 	return (EXIT_SUCCESS);
 }
