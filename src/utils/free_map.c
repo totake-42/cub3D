fea56b4252235132226341fe6329a4be8f4f7f11
Taishi Otake <totake@student.42tokyo.jp>
2026-04-19 11:07:29 +0900

    update: Set to NULL after free

--- src/utils/free_map.c
+++ src/utils/free_map.c
@@ -6,13 +6,37 @@
 /*   By: totake <totake@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/04 13:45:45 by itakumi           #+#    #+#             */
-/*   Updated: 2026/02/20 14:19:32 by totake           ###   ########.fr       */
+/*   Updated: 2026/04/19 11:04:11 by totake           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 #include "libft.h"
 #include <stdlib.h>
+
+static void	free_texture_paths(t_map *map_data)
+{
+	if (map_data->north_tex_path != NULL)
+	{
+		free(map_data->north_tex_path);
+		map_data->north_tex_path = NULL;
+	}
+	if (map_data->south_tex_path != NULL)
+	{
+		free(map_data->south_tex_path);
+		map_data->south_tex_path = NULL;
+	}
+	if (map_data->west_tex_path != NULL)
+	{
+		free(map_data->west_tex_path);
+		map_data->west_tex_path = NULL;
+	}
+	if (map_data->east_tex_path != NULL)
+	{
+		free(map_data->east_tex_path);
+		map_data->east_tex_path = NULL;
+	}
+}
 
 void	free_map(t_map **map_data)
 {
@@ -31,14 +55,7 @@ void	free_map(t_map **map_data)
 		free((*map_data)->grid);
 		(*map_data)->grid = NULL;
 	}
-	if ((*map_data)->north_tex_path != NULL)
-		free((*map_data)->north_tex_path);
-	if ((*map_data)->south_tex_path != NULL)
-		free((*map_data)->south_tex_path);
-	if ((*map_data)->west_tex_path != NULL)
-		free((*map_data)->west_tex_path);
-	if ((*map_data)->east_tex_path != NULL)
-		free((*map_data)->east_tex_path);
+	free_texture_paths(*map_data);
 	free(*map_data);
 	*map_data = NULL;
 }
